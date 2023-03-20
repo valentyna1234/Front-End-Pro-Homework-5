58e2e42e24bad12e6f7b56e4e76ce0a8e8e23a94
valentyna1234 <pisareva.valya@gmail.com>
2023-03-20 18:40:42 +0200

    Add index.js file

---
+++ index.js
@@ -0,0 +1,19 @@
+//Getting values ​​from the user
+let a, b, c;
+a = Number(prompt( 'Enter the first value, please.' ));
+console.log(a);
+b = Number(prompt( 'Enter the second value, please.' ));
+console.log(b);
+c = Number(prompt('And, please, enter a third value.'));
+console.log(c)
+
+//The arithmetic mean of 3 numbers
+let d = ( a + b + c ) / 3;
+console.log(d);
+
+//Outputting results
+alert('The arithmetic mean of these three numbers is ' + d);
+
+
+
+
